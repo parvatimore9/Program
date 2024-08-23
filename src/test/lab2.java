f8e25c22f862826555f4f3df16c4c8a60fcff1da
Parvati <parvatimore9@gmail.com>
2024-08-23 21:21:32 +0530

    changes

--- src/test/lab2.java
+++ src/test/lab2.java
@@ -2,6 +2,6 @@ package test;
 
 public class lab2 {
     public static void main(String[] args) {
-        System.out.println("hello everyone");
+        System.out.println("hello everyonee");
     }
 }
